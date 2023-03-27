97f644831d223ec32070cb4a21159222ad5de193
johnedisc <christopher.johnedis@gmail.com>
2023-03-27 11:00:04 -0700

    set up animal model, context, and update program.cs
    Co-authored-by: Vera Weikel<vera.weikel@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
+using CretaceousApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<CretaceousApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
+else 
 {
-    app.UseHttpsRedirection();
+  app.UseHttpsRedirection();
 }
 
 app.UseAuthorization();
